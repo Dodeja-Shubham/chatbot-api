e5181fcbe87dbc395f954404b85f07c1c362f761
Dodeja-Shubham <dodejashubham@gmail.com>
2020-09-19 19:25:31 +0530

    Update settings.py

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -80,22 +80,22 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'chatbot',
-#         'USER': 'shubham',
-#         'PASSWORD': 'wsx123',
-#         'HOST': '127.0.0.1',
-#     }
-# }
-
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
-
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chatbot',
+        'USER': 'shubham',
+        'PASSWORD': 'wsx123',
+        'HOST': '127.0.0.1',
+    }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 
